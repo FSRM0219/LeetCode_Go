0cce018e7724f7a00d3676e93ddf7ae7670c3f89
XPRJ0219 <fsrm0219@qq.com>
2024-07-08 19:44:31 +0800

    2024.7.8

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"slices"
+)
+
+func groupAnagrams(strs []string) [][]string {
+	// 49字母异位词分组
+	var ans [][]string
+	dict := make(map[string][]string)
+	for _, s := range strs {
+		t := []byte(s)
+		slices.Sort(t)
+		key := string(t)
+		dict[key] = append(dict[key], s)
+	}
+	for _, cur := range dict {
+		ans = append(ans, cur)
+	}
+	return ans
+}
 
 func main() {
-	fmt.Println("")
+
 }
